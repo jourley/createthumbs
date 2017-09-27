3f062343d2233add64ca2064e95e170fa13db4de
green <vbarrios@gmail.com>
2017-09-26 19:01:56 -0500

    cahnge to 100
    change to 100

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var gm = require('gm').subClass({ imageMagick: true }); // Enable ImageMagick in
 var util = require('util');
 
 // constants
-var MAX_WIDTH  = 10;
-var MAX_HEIGHT = 10;
+var MAX_WIDTH  = 100;
+var MAX_HEIGHT = 100;
 
 // get reference to S3 client 
 var s3 = new AWS.S3();
